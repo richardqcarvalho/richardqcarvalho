ac58b5eade209f1ab73328e4b1712401ed62ec1e
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-04-13 12:37:22 -0300

    feat: adding react & vite to project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default () => {
+	return (
+		<div className='container'>
+			<span className='home-message'>I'm working on that.</span>
+			<span className='home-message'>{'Come back later, please :)'}</span>
+			<span className='advice'>
+				(click{' '}
+				<a href='https://github.com/richardqcarvalho/richardqcarvalho'>here</a>{' '}
+				if you wanna take a look on how it's going)
+			</span>
+		</div>
+	)
+}
