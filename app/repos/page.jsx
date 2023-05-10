d4a440169a5b32f688980ddedf3912ad4e3384c9
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-05-10 18:23:27 -0300

    chore: retrieving back data through front

--- app/repos/page.jsx
+++ app/repos/page.jsx
@@ -2,9 +2,16 @@ import Link from 'next/link'
 import React from 'react'
 
 export default async function ReposPage() {
+  const repos = await (
+    await fetch('https://api.github.com/users/richardqcarvalho/repos')
+  ).json()
+
   return (
     <div className='flex flex-col items-center justify-center p-6 bg-red-400'>
       <h1 className='uppercase text-xl'>Repos</h1>
+      {repos.map(repo => (
+        <span id={repo.id}>{repo.name}</span>
+      ))}
       <Link href='/'>Home</Link>
     </div>
   )
