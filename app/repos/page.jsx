64738dc4756318b27d2334d089f53fe4b5f68833
Richard Carvalho <richardcarvalhooo@gmail.com>
2023-05-10 18:09:33 -0300

    chore: small restartings

--- app/repos/page.jsx
+++ app/repos/page.jsx
@@ -1,10 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function ReposPage() {
+export default async function ReposPage() {
   return (
     <div className='flex flex-col items-center justify-center p-6 bg-red-400'>
       <h1 className='uppercase text-xl'>Repos</h1>
-      <a href='/'>Home</a>
+      <Link href='/'>Home</Link>
     </div>
   )
 }
